4290ebd65cad394ffaf6fb0354c898d244b234e8
BhanuKiranChaluvadi <nthdimensionalreality@gmail.com>
2023-12-08 16:54:16 +0000

    feat: print data frames to terminal

--- main.cpp
+++ main.cpp
@@ -1,15 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "yahdlc.h"
 
 int main() {
-    // Initialize a YAHDLC control structure
+    FILE *file = fopen("../transmission.bin", "rb");
+    if (!file) {
+        perror("Error opening transmission.bin");
+        return -1;
+    }
+
+    // Read the entire file into a buffer
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char *buffer = (char *)malloc(file_size);
+    if (!buffer) {
+        perror("Memory allocation failed");
+        fclose(file);
+        return -1;
+    }
+    fread(buffer, 1, file_size, file);
+    fclose(file);
+
+    // Process the buffer
     yahdlc_control_t control;
-    control.frame = YAHDLC_FRAME_DATA;
-    control.seq_no = 1;
+    unsigned int dest_len;
+    char dest[1024];  // Adjust buffer size as needed
 
-    // Print a message to indicate the program is running
-    printf("YAHDLC library integrated successfully. Control Frame: %d, Sequence Number: %d\n", 
-           control.frame, control.seq_no);
+    for (int i = 0; i < file_size;) {
+        int ret = yahdlc_get_data(&control, buffer + i, file_size - i, dest, &dest_len);
+        if (ret < 0) {
+            // Handle error or end of frame
+            break;
+        }
 
+        // Print the decoded frame
+        printf("Frame: Type=%d, Seq No=%d, Data=", control.frame, control.seq_no);
+
+        // Assuming the data is a series of integers
+        for (unsigned int j = 0; j < dest_len; ++j) {
+            printf("%d ", (unsigned char)dest[j]);
+        }
+        printf("\n");
+
+        // Move to the next frame in the buffer
+        i += ret;
+    }
+
+    free(buffer);
     return 0;
 }
