89e09f08a376eb40679b86ccd49d6fdc984a8b9b
BhanuKiranChaluvadi <nthdimensionalreality@gmail.com>
2023-12-09 20:26:26 +0000

    fix: logic for validMove and moveCharacter

--- src/grid_navigation.c
+++ src/grid_navigation.c
@@ -4,38 +4,49 @@
 int characterPosition[2] = {0, 4}; // [row, col]
 
 // Function to check if a move is valid
-int isValidMove(int *x, int *y, int direction) {
-    switch (direction) {
-        case 1: // Up ↑
-            if (*y < GRID_SIZE - 1) return 1;
-            break;
-        case 2: // Down ↓
-            if (*y > 0) return 1;
-            break;
-        case 3: // Right →
-            if (*x < GRID_SIZE - 1) return 1;
-            break;
-        case 4: // Left ←
-            if (*x > 0) return 1;
-            break;
+int isValidMove(int *x, int *y, int direction)
+{
+    switch (direction)
+    {
+    case 1: // Up ↑
+        if (*y > 0)
+            return 1;
+        break;
+    case 2: // Down ↓
+        if (*y < GRID_SIZE - 1)
+            return 1;
+        break;
+    case 3: // Right →
+        if (*x < GRID_SIZE - 1)
+            return 1;
+        break;
+    case 4: // Left ←
+        if (*x > 0)
+            return 1;
+        break;
     }
     return 0;
 }
 
 // Function to move the character on the grid
-void moveCharacter(int *x, int *y, int direction) {
-    switch (direction) {
-        case 1: // Up ↑
-            if (*y < GRID_SIZE - 1) *y += 1;
-            break;
-        case 2: // Down ↓
-            if (*y > 0) *y -= 1;
-            break;
-        case 3: // Right →
-            if (*x < GRID_SIZE - 1) *x += 1;
-            break;
-        case 4: // Left ←
-            if (*x > 0) *x -= 1;
-            break;
+void moveCharacter(int *x, int *y, int direction)
+{
+    if (!isValidMove(x, y, direction))
+        return;
+
+    switch (direction)
+    {
+    case 1: // Up ↑
+        *y -= 1;
+        break;
+    case 2: // Down ↓
+        *y += 1;
+        break;
+    case 3: // Right →
+        *x += 1;
+        break;
+    case 4: // Left ←
+        *x -= 1;
+        break;
     }
 }
--- tests/test_navigation.cpp
+++ tests/test_navigation.cpp
@@ -1,17 +1,20 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE gridnavigator
 #include <boost/test/unit_test.hpp>
-extern "C" {
-    #include "grid_navigation.h"
+extern "C"
+{
+#include "grid_navigation.h"
 }
 
 #define GRID_SIZE 5
 
-BOOST_AUTO_TEST_CASE(nothingTest) {
+BOOST_AUTO_TEST_CASE(nothingTest)
+{
     BOOST_CHECK(true); // This is a placeholder test case. You can replace it with your own test.
 }
 
-BOOST_AUTO_TEST_CASE(isValidMoveTest) {
+BOOST_AUTO_TEST_CASE(isValidMoveTest)
+{
     int x = 2;
     int y = 2;
 
@@ -31,4 +34,82 @@ BOOST_AUTO_TEST_CASE(isValidMoveTest) {
     y = GRID_SIZE - 1;
     BOOST_CHECK_EQUAL(isValidMove(&x, &y, 2), 0); // Down ↓ (out of bounds)
     BOOST_CHECK_EQUAL(isValidMove(&x, &y, 3), 0); // Right → (out of bounds)
+
+    x = 0;
+    y = 4;
+    BOOST_CHECK_EQUAL(isValidMove(&x, &y, 1), 1); // Up ↑
+    BOOST_CHECK_EQUAL(isValidMove(&x, &y, 2), 0); // Down ↓ (out of bounds)
+    BOOST_CHECK_EQUAL(isValidMove(&x, &y, 3), 1); // Right →
+    BOOST_CHECK_EQUAL(isValidMove(&x, &y, 4), 0); // Left ← (out of bounds)
+}
+
+// write a test case for moveCharacter
+BOOST_AUTO_TEST_CASE(moveCharacterTest)
+{
+    int x = 2;
+    int y = 2;
+
+    // Test valid moves
+    moveCharacter(&x, &y, 1); // Up ↑
+    BOOST_CHECK_EQUAL(x, 2);
+    BOOST_CHECK_EQUAL(y, 1);
+
+    moveCharacter(&x, &y, 2); // Down ↓
+    BOOST_CHECK_EQUAL(x, 2);
+    BOOST_CHECK_EQUAL(y, 2);
+
+    moveCharacter(&x, &y, 3); // Right →
+    BOOST_CHECK_EQUAL(x, 3);
+    BOOST_CHECK_EQUAL(y, 2);
+
+    moveCharacter(&x, &y, 4); // Left ←
+    BOOST_CHECK_EQUAL(x, 2);
+    BOOST_CHECK_EQUAL(y, 2);
+
+    // Test invalid moves
+    x = 0;
+    y = 0;
+    moveCharacter(&x, &y, 1); // Up ↑ (out of bounds)
+    BOOST_CHECK_EQUAL(x, 0);
+    BOOST_CHECK_EQUAL(y, 0);
+
+    moveCharacter(&x, &y, 4); // Left ← (out of bounds)
+    BOOST_CHECK_EQUAL(x, 0);
+    BOOST_CHECK_EQUAL(y, 0);
+
+    x = GRID_SIZE - 1;
+    y = GRID_SIZE - 1;
+    moveCharacter(&x, &y, 2); // Down ↓ (out of bounds)
+    BOOST_CHECK_EQUAL(x, GRID_SIZE - 1);
+    BOOST_CHECK_EQUAL(y, GRID_SIZE - 1);
+
+    moveCharacter(&x, &y, 3); // Right → (out of bounds)
+    BOOST_CHECK_EQUAL(x, GRID_SIZE - 1);
+    BOOST_CHECK_EQUAL(y, GRID_SIZE - 1);
+
+    x = 0;
+    y = 4;
+
+    // Test invalid move
+    moveCharacter(&x, &y, 2); // Down ↓ (out of bounds)
+    BOOST_CHECK_EQUAL(x, 0);
+    BOOST_CHECK_EQUAL(y, 4);
+
+    // Test invalid move
+    moveCharacter(&x, &y, 4); //  Left ← (out of bounds)
+    BOOST_CHECK_EQUAL(x, 0);
+    BOOST_CHECK_EQUAL(y, 4);
+
+    // Reset position
+    x = 0;
+    y = 4;
+
+    // Test invalid moves
+    moveCharacter(&x, &y, 1); // Up ↑
+    BOOST_CHECK_EQUAL(x, 0);
+    BOOST_CHECK_EQUAL(y, 3);
+
+    moveCharacter(&x, &y, 4); // Left ← (out of bounds)
+    BOOST_CHECK_EQUAL(x, 0);
+    BOOST_CHECK_EQUAL(y, 3);
 }
